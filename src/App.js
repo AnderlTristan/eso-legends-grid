1bfa20fafa6a724bb39b168c6b708a277f3a374d
Tristan Anderl <tristananderl@Tristans-MacBook-Pro.local>
2021-05-21 07:31:48 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+import React, { useState, useRef, useCallback } from 'react'
+// components
+import { Card } from './components/Card'
+import { Loader } from './components/Loader';
+// hooks
+import useFetch from './hooks/useFetch'
+// css
+import styled from 'styled-components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [query, setQuery] = useState('');
+	const [pageNumber, setPageNumber] = useState(1);
+ 
+	// destructure variables returned from data fetch hook
+	// useFetch(<query param>, <page number>, <request limit>)
+	const { loading, error, cards, hasMore } = useFetch(query, pageNumber, 20);
+
+	// Setting up hook for intersection observer which will let React know when we've scrolled to the last currently rendered element
+	const observer = useRef();
+	const lastCardElement = useCallback(node => {
+		if(loading) return; 
+		// update observer with new element
+		if(observer.current) observer.current.disconnect();
+		// set observe reference to the last currenlty rendered element (moving to next page of API)
+		observer.current = new IntersectionObserver(entries => {
+			if(entries[0].isIntersecting && hasMore) {
+				setPageNumber(prevPageNumber => prevPageNumber + 1);
+			}
+		})
+		if(node) observer.current.observe(node);
+	}, [loading, hasMore]);
+
+	// handling input query
+	function handleSearch(e) {
+		setQuery(e.target.value);
+		setPageNumber(1);
+	}
+	
+	return (
+		<div className="App">
+			<div className="textInput">
+				<label className="textInput__label">Search for card</label>
+				<input className="textInput__input" type="text" onChange={handleSearch} value={query} placeholder="Card name"></input>
+			</div>
+			<CardContainer>
+				{cards.map((card, index) => {
+					// check if current index is last index of most recent query
+					// if last index, set ref attribute to use in useCallback hook
+					if(cards.length === index + 1) {
+						return (
+							// wrap Card components in divs so that ref attribute can be used
+							<div ref={lastCardElement} key={index}>
+								<Card 
+									name={card.name}
+									type={card.type}
+									imageUrl={card.imageUrl}
+									setName={card.set.name}
+									text={card.text}
+								/>
+							</div>
+						)
+					// if not last index, render component normally
+					} else {
+						return (
+							<div key={index}>
+								<Card 
+									name={card.name}
+									type={card.type}
+									imageUrl={card.imageUrl}
+									setName={card.set.name}
+									text={card.text}
+								/>
+							</div>
+						)
+					}
+				})}
+				<div>{loading && <Loader/>}</div>
+				<div>{error && 'Error...'}</div>
+			</CardContainer>
+		</div>
+	);
+};
+
+const CardContainer = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	background: #e0e5eb;
+	padding: 3% 0;
+	position: relative;
+	z-index: 5;
+`;
 
 export default App;
+
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+
+export default function useFetch(query, pageNumber, limit) {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [cards, setCards] = useState([]);
+    const [hasMore, setHasMore] = useState(false);
+
+    // Prevent old search results from displaying as query gets longer
+    useEffect(() => {
+        setCards([])
+    }, [query]);
+    
+    // Fetch data based on search query
+    useEffect(() => {
+        let cancel;
+        setLoading(true);
+        setError(false);
+
+        axios({
+            method: 'GET',
+            url: `https://api.elderscrollslegends.io/v1/cards`,
+            params: { name: query, page: pageNumber, pageSize: limit },
+            cancelToken: new axios.CancelToken(c => cancel = c)
+        }).then(res => {
+            // add card from api response to existing cards
+            setCards(prevCards => {
+                return [...prevCards, ...res.data.cards];
+            });
+            // set hasMore flag to true only if there are more entries to get from api
+            setHasMore(res.data.cards.length > 0);
+            setLoading(false);
+        }).catch(e => {
+            if(axios.isCancel(e)) return;
+            setError(true);
+        });
+        return () => cancel();
+    }, [query, pageNumber, limit]);
+
+    return { loading, error, cards, hasMore }
+}
